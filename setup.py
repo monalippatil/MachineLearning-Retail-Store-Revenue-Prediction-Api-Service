6c370603224d2b2a10a6392f1aa5c307b7162091
Monali Patil <monalipatil@Monalis-MacBook-Pro.local>
2023-10-06 18:23:00 +1100

    Git Initialization

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Constructing a Machine Learning Predictive and Forecasting Models as a Service.',
+    author='Monali',
+    license='MIT',
+)
